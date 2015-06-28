c6346ac7bc5e04eb94e0e8929d3b028f9d1d4254
abysmli <abysmli@gmail.com>
2015-06-28 20:22:05 +0200

    Initial Commit

---
+++ MongoX/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MongoX
+//
+//  Created by AbysmLi on 6/28/15.
+//  Copyright (c) 2015 AbysmLi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
