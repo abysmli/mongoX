6574031ae185848b34fbcb24a6c57b7a57b5e0fd
abysmli <abysmli@gmail.com>
2015-06-29 09:39:24 +0200

    init

---
+++ MongoX-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  MongoX-Bridging-Header.h
+//  MongoX
+//
+//  Created by AbysmLi on 6/29/15.
+//  Copyright (c) 2015 AbysmLi. All rights reserved.
+//
+
+#ifndef MongoX_MongoX_Bridging_Header_h
+#define MongoX_MongoX_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+#import <KASlideShow/KASlideShow.h>
+
+#endif
